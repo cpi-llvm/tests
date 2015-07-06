450739f6cbcad88ec5d44f18d81b177fce30d071
Volodymyr Kuznetsov <ks.vladimir@gmail.com>
2015-07-06 14:07:53 +0200

    Added tests with dlopen and pthread

---
+++ safestack/dlopen-pthread-pre.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <dlfcn.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#include "lib.h"
+
+pthread_mutex_t mutex;
+
+static void *start(void *str)
+{
+  char buffer[1024];
+
+  snprintf(buffer, sizeof (buffer), "thread in %s\n", str);
+  printf("%s", buffer);
+  pthread_mutex_lock(&mutex);
+  return NULL;
+}
+
+int main(int argc, char **argv)
+{
+  void *handle = dlopen(SHARED_LIBRARY, RTLD_NOW | RTLD_LOCAL);
+  if (!handle) {
+    fprintf(stderr, "dlopen(%s): %s\n", SHARED_LIBRARY, dlerror());
+    return 1;
+  }
+
+  pthread_mutex_init(&mutex, NULL);
+  pthread_mutex_lock(&mutex);
+
+  pthread_t thread;
+  if (pthread_create(&thread, NULL, start, argv[0])) {
+    fprintf(stderr, "pthread_create(): %m\n");
+    return 1;
+  }
+
+  union {
+    void *addr;
+    void *(*fn)(void *);
+  } sym;
+
+  sym.addr = dlsym(handle, "hello");
+  if (!sym.addr) {
+    fprintf(stderr, "%s: symbol 'hello' not found\n", SHARED_LIBRARY);
+    dlclose(handle);
+    return 1;
+  }
+
+  sym.fn(argv[0]);
+
+  dlclose(handle);
+
+  pthread_mutex_unlock(&mutex);
+  pthread_join(thread, NULL);
+  pthread_mutex_destroy(&mutex);
+  return 0;
+}
